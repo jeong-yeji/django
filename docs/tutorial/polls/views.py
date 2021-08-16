85a2d0395951cbed708a1500456ff0607101d9f2
jeong-yeji <jeongyeji.mail@gmail.com>
2021-08-16 16:14:47 +0900

    django tutorial - part4

--- docs/tutorial/polls/views.py
+++ docs/tutorial/polls/views.py
@@ -1,11 +1,16 @@
 from django import template
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import context, loader
-from .models import Question
+from django.urls import reverse
+from django.views import generic
+from .models import Choice, Question
+from polls import models
 
 # Create your views here.
 
+"""
+# view를 직접 만들어서 사용
 
 def index(request):
     # 1
@@ -52,9 +57,61 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    # 1
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+"""
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # 1
+    # return HttpResponse("You're voting on question %s." % question_id)
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didnt' select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,))
+        )  # /polls/question.id/results/
+        # reverse()의 사용으로 view에서 URL 하드 코딩 방지
+
+
+# generic view 사용
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        # return the last five published questions.
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    # URL에서 받을 기본키 값을 'pk'라고 기대하기 때문에 urls.py에서 변경
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
