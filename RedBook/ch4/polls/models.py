a679e5baa5ac5a51331688ab5b8d098be4ed3fb6
jeong-yeji <jeongyeji.mail@gmail.com>
2021-08-21 16:46:17 +0900

    수정 및 ch4 생성

---
+++ RedBook/ch4/polls/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+# PK(id)는 Django에서 자동 생성됨 - Not NULL, Autoincrement
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
