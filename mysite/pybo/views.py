618932667074a416fd419a95d94b48670ac9ab3c
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-05 00:53:23 +0900

    03-7. add author to model

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,6 +4,7 @@ from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -22,6 +23,9 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+# 로그인 상태인지 확인 후, 로그아웃 상태이면 common:login으로 이동
+# next 파라미터를 가지고 이동하므로 로그인 후 원래 페이지로 이동 설정 가능
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
@@ -31,6 +35,7 @@ def answer_create(request, question_id):
             answer = form.save(commit=False)
             answer.create_date = timezone.now()
             answer.question = question
+            answer.author = request.user
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
@@ -39,12 +44,14 @@ def answer_create(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST': # POST 방식
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
             question.create_date = timezone.now()
+            question.author = request.user
             question.save()
             return redirect('pybo:index')
     else: # GET 방식
