9a29597abe9f5ae5ef3948c22417bd1851ec33bf
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-06 00:16:04 +0900

    03-9 (1) 질문 수정

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,3 +1,4 @@
+from typing import ContextManager
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question, Answer
@@ -5,6 +6,7 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -56,5 +58,28 @@ def question_create(request):
             return redirect('pybo:index')
     else: # GET 방식
         form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다') # non-field error
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else: # GET 방식
+        # 기존 내용 반영해서 폼 생성
+        form = QuestionForm(instance=question)
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
