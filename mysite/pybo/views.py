08619b40ad7941e42c114c25b8c1c7aeb5226332
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-07 03:47:43 +0900

    03-10 (2) 답변 댓글 기능

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -23,27 +23,7 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
-# 로그인 상태인지 확인 후, 로그아웃 상태이면 common:login으로 이동
-# next 파라미터를 가지고 이동하므로 로그인 후 원래 페이지로 이동 설정 가능
-@login_required(login_url='common:login')
-def answer_create(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-
-    if request.method == 'POST':
-        form = AnswerForm(request.POST)
-        if form.is_valid():
-            answer = form.save(commit=False)
-            answer.create_date = timezone.now()
-            answer.question = question
-            answer.author = request.user
-            answer.save()
-            return redirect('pybo:detail', question_id=question.id)
-    else:
-        form = AnswerForm()
-    context = {'question': question, 'form': form}
-    return render(request, 'pybo/question_detail.html', context)
-
-
+## 질문 관련 함수
 @login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST': # POST 방식
@@ -94,6 +74,28 @@ def question_delete(request, question_id):
     return redirect('pybo:index')
 
 
+## 답변 관련 함수
+# 로그인 상태인지 확인 후, 로그아웃 상태이면 common:login으로 이동
+# next 파라미터를 가지고 이동하므로 로그인 후 원래 페이지로 이동 설정 가능
+@login_required(login_url='common:login')
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.author = request.user
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+
 @login_required(login_url='common:login')
 def answer_modify(request, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
@@ -127,6 +129,7 @@ def answer_delete(request, answer_id):
     return redirect('pybo:detail', question_id=answer.question.id)
 
 
+## 질문 댓글 관련 함수
 @login_required(login_url='common:login')
 def comment_create_question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -179,3 +182,58 @@ def comment_delete_question(request, comment_id):
     else:
         comment.delete()
     return redirect('pybo:detail', question_id=comment.question_id)
+
+
+## 답변 댓글 관련 함수
+@login_required(login_url='common:login')
+def comment_create_answer(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.create_date = timezone.now()
+            comment.answer = answer
+            comment.save()
+            # 댓글 저장 후, 댓글을 작성한 질문 상세 화면으로 redirect
+            return redirect('pybo:detail', question_id=comment.answer.question.id)
+    else: # GET
+        form = CommentForm()
+    context = {'form': form}
+    return render(request, 'pybo/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_modify_answer(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    
+    if request.user != comment.author:
+        messages.error(request, '댓글수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=comment.answer.question.id)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.modify_date = timezone.now()
+            comment.save()
+            return redirect('pybo:detail', question_id=comment.answer.question.id)
+    else: # GET
+        form = CommentForm(instance=comment)
+    context = {'form': form}
+    return render(request, 'pybo/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_delete_answer(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.user != comment.author:
+        messages.error(request, '댓글삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id=comment.answer.question_id)
+    else:
+        comment.delete()
+    return redirect('pybo:detail', question_id=comment.answer.question_id)
