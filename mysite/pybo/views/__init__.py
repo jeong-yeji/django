825bd40ea2e34a4789702dfc7118761268072646
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-07 15:41:57 +0900

    03-11 (2) views.py 분리 - 모듈명 표시

--- mysite/pybo/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base_views import *
-from .question_views import *
-from .answer_views import *
-from .comment_views import *
-
-# 모든 함수를 import하므로 views.py 파일을 사용할 때와 차이 없음
-# => views module을 import하여 사용하는 파일들을 수정할 필요 없음
