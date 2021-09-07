9ec695f0a135b984efe407711ac5449717dd6d22
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-08 02:20:16 +0900

    03-12 (2) 답변 추천

--- mysite/pybo/views/vote_views.py
+++ mysite/pybo/views/vote_views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
 
-from ..models import Question
+from ..models import Question, Answer
 
 ## 추천 관리 ##
 
@@ -17,3 +17,15 @@ def vote_question(request, question_id):
         question.voter.add(request.user)
         # ManyToManyField는 중복X => 여러번 추천해도 추천 수는 증가하지 않음
     return redirect('pybo:detail', question_id=question.id)
+
+
+# 답변 추천
+@login_required(login_url='common:login')
+def vote_answer(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
+    else:
+        answer.voter.add(request.user)
+    return redirect('pybo:detail', question_id=answer.question.id)
