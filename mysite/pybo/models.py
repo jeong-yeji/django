bc6a41d8e579641c63697fd04e7f09937c44f7b8
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-08 02:19:54 +0900

    03-12 (1) 질문 추천

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -8,7 +8,13 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    voter = models.ManyToManyField(User, related_name='voter_question')
+    # author와 voter가 모두 User 모델을 참조하므로
+    # User.question_set과 같이 User 모델을 통해 Question 데이터에 접근할 경우 어떤걸 참조할지 지정해야됨
+    # 특정 사용자가 작성한 질문을 얻는 코드 : some_user.author_question.all()
+    # 특정 사용자가 추천한 질문을 얻는 코드 : some_user.voter_question.all()
+
 
     def __str__(self):
         return self.subject
@@ -19,7 +25,8 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 
 class Comment(models.Model):
---
+++ mysite/pybo/views/vote_views.py
@@ -0,0 +1,19 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect
+
+from ..models import Question
+
+## 추천 관리 ##
+
+# 질문 추천
+@login_required(login_url='common:login')
+def vote_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    if request.user == question.author:
+        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
+    else: # ManyToManyField => add()
+        question.voter.add(request.user)
+        # ManyToManyField는 중복X => 여러번 추천해도 추천 수는 증가하지 않음
+    return redirect('pybo:detail', question_id=question.id)
