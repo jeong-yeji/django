af731f0ca0054ed6b17401d2166dfb27003f4f23
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-10 02:38:09 +0900

    3-14. markdown

--- mysite/pybo/templatetags/pybo_filter.py
+++ mysite/pybo/templatetags/pybo_filter.py
@@ -1,4 +1,6 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -7,3 +9,12 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+# markdown filter
+# nl2br : 줄바꿈문자를 <br> 태그로 바꿔줌 >> 엔터 한번만 눌러도 줄바꿈으로 인식
+# fenced_code : 마크다운의 소스 코드 표현
+@register.filter()
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
