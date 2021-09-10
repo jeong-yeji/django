505c4f1675ae1769c013a7f165ab3c64cbdd7eda
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-11 00:01:53 +0900

    3-15 (1) 검색 기능

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,16 +1,33 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
 ## 기본 관리 ##
 
 def index(request):
-    page = request.GET.get('page', '1') # url에서 값 가져오되, 기본값 1
+    # parameter
+    page = request.GET.get('page', '1')  # page, 기본값 1
+    kw = request.GET.get('kw', '')       # 검색어
+
+    # 검색
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            # Q() : OR 조건으로 데이터 조회
+            # contains vs icontains : 대소문자 구별O vs 구별X
+            Q(subject__icontains=kw) |                  # 제목
+            Q(content__icontains=kw) |                  # 내용
+            Q(author__username__icontains=kw) |         # 질문 글쓴이
+            Q(answer__author__username__icontains=kw)   # 답변 글쓴이
+        ).distinct() # 중복 제거
+
+    # pagination
     paginator = Paginator(question_list, 10) # 10 questions per page
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 
