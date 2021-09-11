aa3515d9387e4b5373dbbe5614fff544aa0ad99d
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-11 16:11:57 +0900

    3-15 (2) 정렬 기능

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from ..models import Question
 
@@ -10,9 +10,19 @@ def index(request):
     # parameter
     page = request.GET.get('page', '1')  # page, 기본값 1
     kw = request.GET.get('kw', '')       # 검색어
+    so = request.GET.get('so', 'recent') # 정렬 기준
+
+    # 정렬
+    # annotate() : 임의의 필드를 임시로 추가해주는 함수 > filter(), order_by()에서 사용 가능
+    # order_by의 기준이 여러개인 경우, 첫번째 항목부터 우선순위를 매김
+    if so == 'recommend': # 추천순
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular': # 인기순 : 답변이 많이 달린 순으로
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else: # default: 최신순
+        question_list = Question.objects.order_by('-create_date')
 
     # 검색
-    question_list = Question.objects.order_by('-create_date')
     if kw:
         question_list = question_list.filter(
             # Q() : OR 조건으로 데이터 조회
@@ -27,7 +37,7 @@ def index(request):
     paginator = Paginator(question_list, 10) # 10 questions per page
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 
