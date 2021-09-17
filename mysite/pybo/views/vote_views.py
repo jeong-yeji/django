ca3e51fa8a81863f0806df6a252a2a136cc0ce0e
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-18 00:10:57 +0900

    modify model - comment voter

--- mysite/pybo/views/vote_views.py
+++ mysite/pybo/views/vote_views.py
@@ -1,8 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, resolve_url
 
-from ..models import Question, Answer
+from ..models import Comment, Question, Answer
 
 ## 추천 관리 ##
 
@@ -28,4 +28,20 @@ def vote_answer(request, answer_id):
         messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
     else:
         answer.voter.add(request.user)
-    return redirect('pybo:detail', question_id=answer.question.id)
+    return redirect('{}#answer_{}'.format(resolve_url('pybo:detail', question_id=answer.question.id), answer_id))
+
+
+# 댓글 추천
+@login_required(login_url='common:login')
+def vote_comment(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.user == comment.author:
+        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
+    else:
+        comment.voter.add(request.user)
+    
+    if comment.question:
+        return redirect('{}#comment_{}'.format(resolve_url('pybo:detail', question_id=comment.question.id), comment_id))
+    else: # if comment.answer
+        return redirect('{}#comment_{}'.format(resolve_url('pybo:detail', question_id=comment.answer.question.id), comment_id))
