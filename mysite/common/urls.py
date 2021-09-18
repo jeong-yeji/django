9e31fa8526c034e47191d97516dc3ab72bf7cf86
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-19 00:47:22 +0900

    user information

--- mysite/common/urls.py
+++ mysite/common/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+from .views import common_views, profile_views
 
 app_name = 'common'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
+    path('signup/', common_views.signup, name='signup'),
+
+    # profile
+    path('profile/base/<int:user_id>/', profile_views.base, name='profile_base'),
+    path('profile/question/<int:user_id>/', profile_views.question, name='profile_question'),
+    path('profile/answer/<int:user_id>/', profile_views.answer, name='profile_answer'),
+    path('profile/comment/<int:user_id>/', profile_views.comment, name='profile_comment'),
 ]
---
+++ mysite/common/views/profile_views.py
@@ -0,0 +1,81 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
+
+from django.contrib.auth.models import User
+from pybo.models import Question, Answer, Comment
+
+@login_required(login_url='common:login')
+def base(request, user_id):
+    # parameter
+    user = get_object_or_404(User, pk=user_id)
+
+    context = {'user': user, 'profile_type': 'base'}
+    return render(request, 'common/profile_base.html', context)
+
+
+@login_required(login_url='common:login')
+def question(request, user_id):
+    # parameter
+    user = get_object_or_404(User, pk=user_id)
+    page = request.GET.get('page', '1')
+    so = request.GET.get('so', 'recent')
+
+    # 정렬
+    question_list = Question.objects.filter(author=user)
+    if so == 'recommend':
+        question_list = question_list.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    else: # recent
+        question_list = question_list.order_by('-create_date')
+
+    # pagination
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    
+    context = {'user': user, 'profile_type': 'question', 'question_list': page_obj, 'page': page}
+    return render(request, 'common/profile_question.html', context)
+
+
+@login_required(login_url='common:login')
+def answer(request, user_id):
+    # parameter
+    user = get_object_or_404(User, pk=user_id)
+    page = request.GET.get('page', '1')
+    so = request.GET.get('so', 'recent')
+
+    # 정렬
+    answer_list = Answer.objects.filter(author=user)
+    if so == 'recommend':
+        answer_list = answer_list.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    else: # recent
+        answer_list = answer_list.order_by('-create_date')
+
+    # pagination
+    paginator = Paginator(answer_list, 10)
+    page_obj = paginator.get_page(page)
+    
+    context = {'user': user, 'profile_type': 'answer', 'answer_list': page_obj, 'page': page}
+    return render(request, 'common/profile_answer.html', context)
+
+
+@login_required(login_url='common:login')
+def comment(request, user_id):
+    # parameter
+    user = get_object_or_404(User, pk=user_id)
+    page = request.GET.get('page', '1')
+    so = request.GET.get('so', 'recent')
+
+    # 정렬
+    comment_list = Comment.objects.filter(author=user)
+    if so == 'recommend':
+        comment_list = comment_list.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    else: # recent
+        comment_list = comment_list.order_by('-create_date')
+
+    # pagination
+    paginator = Paginator(comment_list, 10)
+    page_obj = paginator.get_page(page)
+    
+    context = {'user': user, 'profile_type': 'comment', 'comment_list': page_obj, 'page': page}
+    return render(request, 'common/profile_comment.html', context)
