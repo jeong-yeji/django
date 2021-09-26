9152bd2ab701fa64dd23135e8e6ddb5839dd29ac
jeong-yeji <jeongyeji.mail@gmail.com>
2021-09-26 18:43:31 +0900

    4-11. create static root

--- mysite/config/settings/prod.py
+++ mysite/config/settings/prod.py
@@ -3,3 +3,7 @@ from .base import *
 # 서버 환경
 
 ALLOWED_HOSTS = ['3.35.43.149']
+
+# set default path of static file
+STATIC_ROOT = BASE_DIR / 'static/' # /home/ubuntu/projects/mysite
+STATICFILES_DIRS = [] # 오류 방지용
